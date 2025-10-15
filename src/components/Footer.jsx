356eeedc257969e7d98f0c4a46d766627fd1d682
JCesar206 <jcesar206@hotmail.com>
2025-10-15 17:42:28 -0600

    Se actualiza componente Footer y se agrega archivo ci-cd.yml para deployar la app

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,24 @@
-import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaEnvelope, FaHome } from 'react-icons/fa';
+import { SiGmail } from "react-icons/si";
 
 function Footer() {
     return(
         <footer className="bg-gray-800 text-white text-center p-4 mt-auto">
             <div className='flex justify-center gap-6 items-center mb-2'>
+            <a href="https://jcesar206.github.io/myPersonalBlog/" target="_blank" className="hover:text-lime-400">
+                <FaHome size={20} title="Home Page"/>
+            </a>
             <a href="https://github.com/JCesar206" target="_blank" className="hover:text-cyan-400">
-                <FaGithub size={20}/>
+                <FaGithub size={20} title="Github"/>
             </a>
             <a href="https://www.linkedin.com/in/jcesar206" target="_blank" className="hover:text-blue-400">
-                <FaLinkedin size={20}/>
+                <FaLinkedin size={20} title="Linkedin"/>
             </a>
             <a href="mailto:jcesar206@hotmail.com" className="hover:text-pink-400">
-                <FaEnvelope size={20}/>
+                <FaEnvelope size={20} title="Hotmail"/>
+            </a>
+            <a href="mailto:jcesary06@gmail.com" className="hover:text-red-400">
+                <SiGmail size={20} title="Gmail"/>
             </a>
             </div>
             <p className='text-sm text-gray-400'>
